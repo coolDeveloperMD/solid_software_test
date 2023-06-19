6de384d74e71a75cce047b717fffdc167d64856b
Mayur <allthings6041@gmail.com>
2023-06-19 08:50:24 +0530

    code formatting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,5 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const App());
-
   });
 }
